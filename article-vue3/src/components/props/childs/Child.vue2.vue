31c18bb7fa6fcaa07f9fd29b6d2c951028d5d9c5
Diego Assia <diegoassia@gmail.com>
2021-04-25 19:34:11 -0500

    Initial examples

---
+++ article-vue3/src/components/props/childs/Child.vue2.vue
@@ -0,0 +1,28 @@
+<template>
+    <p> {{ msg }}</p>
+    <input v-model="dataInput" />
+    <button @click="onUpdateValue()"> change value </button>
+</template>
+
+<script>
+export default {
+    name: 'ChildVue3',
+    props: {
+        msg: {
+            type: String,
+            required: true
+        }
+    },
+    data () {
+        return {
+            dataInput: ''
+        }
+    },
+    methods: {
+        onUpdateValue() {
+            this.$emit("updateMsg", this.dataInput)
+            alert('The text has been updated by child')
+        }
+    }
+}
+</script>
